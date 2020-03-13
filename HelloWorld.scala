7ab819cc9318ac96105ecb3f5a3a3b39dafc7b8c
Muhammad Anis <anisiconic@gmail.com>
2020-03-13 14:25:58 +0700

    add package

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,3 +1,5 @@
+import my.anis.stuff._
+
 object HelloWorld {
     def main(args: Array[String]) {
       println("Hello, world!")
---
+++ my.anis.stuff/Car.scala
@@ -0,0 +1,9 @@
+package my.anis.stuff
+
+class Car(wheels: Int) {
+    var wheels: Int = wheels
+
+    def printWheels() {
+        println("Wheels: " + wheels)
+    }
+}
